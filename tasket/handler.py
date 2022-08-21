d009cd3b2670a1de8a3ec6831da6fffc46c6df7d
JObog <jobogbaimhe@gmail.com>
2022-08-21 15:49:26 -0400

    implementing basic task handler method

--- tasket/handler.py
+++ tasket/handler.py
@@ -1,9 +1,18 @@
-from typing import Callable
+from typing import Callable, Tuple
 from .base import Client
+from .task import Task
+import json
+
+TaskFunc = Callable[[Task], Tuple[bytes, bool]]
 
 class Handler(Client):
-    def __init__(self, url: str, channel: str, key: str, callback: Callable):
+    def __init__(self, url: str, channel: str, key: str, fn: TaskFunc):
         super().__init__(url, "HANDLER", channel, key)
+        self._client.on("message", self._handle_task)
+        self._fn = fn
 
-    def _handle_ack(ack_msg: bytes):
-        pass
+    def _handle_task(self, payload: bytes) -> bytes:
+        task = Task.from_json(payload)
+        res, success = self._fn(task)
+        return bytes(json.dumps({"res": res, "success": success}))
+
