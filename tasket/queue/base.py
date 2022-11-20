a99be4e1674a7dec3ae5eece28470dc7af344244
JObog <jobogbaimhe@gmail.com>
2022-11-20 00:11:46 -0500

    adding base abc for queue

---
+++ tasket/queue/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+@dataclass
+class Message:
+    id: str
+    data: bytes
+    timestamp: int
+
+
+class Queue:
+    @abstractmethod
+    def push(self, message: Message):
+        ...
+    
+    @abstractmethod
+    def pull(self) -> Message:
+        ...
